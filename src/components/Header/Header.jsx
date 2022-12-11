a9a2395b836f7d17deef34354b037cf990ee392f
Nexitd <kern45061@gmail.com>
2022-12-11 11:30:11 +0300

    test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,52 +5,57 @@ import { Button } from "antd";
 import styles from "./Header.module.css";
 
 const Header = () => {
-  return (
-    <div className={styles.header}>
-      <div className={styles.headerCont}>
-        <div className={styles.logo}>
-          <img src={logo} alt='' />
-        </div>
-        <div className={styles.menu}>
-          <ul className={styles.list}>
-            <li className={styles.listItem}>
-              <Link className={styles.listLink} to='/'>
-                Главная
-              </Link>
-            </li>
-            <li className={styles.listItem}>
-              <Link className={styles.listLink} to='/about'>
-                О нас
-              </Link>
-            </li>
-            <li className={styles.listItem}>
-              <Link className={styles.listLink} to='/articles'>
-                Наши Статьи
-              </Link>
-            </li>
-            <li className={styles.listItem}>
-              <Link className={styles.listLink} to='/anket'>
-                Подбор программы
-              </Link>
-            </li>
-            <li className={styles.listItem}>
-              <Link className={styles.listLink} to='/price'>
-                Курсы
-              </Link>
-            </li>
-            <li className={styles.listItem}>
-              <Link className={styles.listLink} to='/sertifites'>
-                Сертификаты
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.login}>
-          <Button>Войти</Button>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.header}>
+			<div className={styles.headerCont}>
+				<div className={styles.headerContSurname}>
+					<h3 className={styles.headerContTitle}>Шапранов Никита ВИС-44</h3>
+				</div>
+				<div className={styles.headerContItem}>
+					<div className={styles.logo}>
+						<img src={logo} alt="" />
+					</div>
+					<div className={styles.menu}>
+						<ul className={styles.list}>
+							<li className={styles.listItem}>
+								<Link className={styles.listLink} to="/">
+									Главная
+								</Link>
+							</li>
+							<li className={styles.listItem}>
+								<Link className={styles.listLink} to="/about">
+									О нас
+								</Link>
+							</li>
+							<li className={styles.listItem}>
+								<Link className={styles.listLink} to="/articles">
+									Наши Статьи
+								</Link>
+							</li>
+							<li className={styles.listItem}>
+								<Link className={styles.listLink} to="/anket">
+									Подбор программы
+								</Link>
+							</li>
+							<li className={styles.listItem}>
+								<Link className={styles.listLink} to="/price">
+									Курсы
+								</Link>
+							</li>
+							<li className={styles.listItem}>
+								<Link className={styles.listLink} to="/sertifites">
+									Сертификаты
+								</Link>
+							</li>
+						</ul>
+					</div>
+					<div className={styles.login}>
+						<Button>Войти</Button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
